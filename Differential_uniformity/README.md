3cb3d3163ab9c63932fc3c5879e0499631ae8456
Mariann Lepsøy <huj017@uib.no>
2023-11-28 10:46:30 +0100

    differential uniformity

---
+++ Differential_uniformity/README.md
@@ -0,0 +1,4 @@
+### Overview
+Implementation of a differential uniformity calculator for (n, n)-functions represented as lookup tables. The calculator is designed to compute the differential uniformity of a given function, which is a measure of its cryptographic strength.
+
+The program `du.py` takes a lookup table as input and outputs the differential uniformity of the function.
---
+++ Differential_uniformity/du.py
@@ -0,0 +1,50 @@
+import sys
+
+# Finds the differential uniformity
+def differential_uniformity(truth_table):
+    n = len(truth_table[0][0]) # Length of bits
+    max_diff = 0 # After the loop below finish running, this will be the differential uniformity.
+    for i in range(1, 2**n): # Goes through all possible input differences except for 0
+        a_str = format(i, '06b') # Turn the integer into a string
+        a = string_to_bits(a_str) # Turn the string into a list of bits
+        dict_count = {} # Keeps track of all output differences and how many times they occur for the current input difference
+        for x, fx in truth_table:
+            for y, fy in truth_table:
+                if xor(x, y) == a:
+                    b = bits_to_string(xor(fx, fy)) # Find output difference 
+                    dict_count[b] = dict_count.get(b, 0) + 1 # Add output difference as key and 1 as value if it is first occurence. Add 1 to the value if output difference is already in dict.
+        l = list(dict_count.values()) # Get the values as list so it can be sorted in descending order and thereafter get the first number in the list as that is the highest occurence of an output difference for the current input difference.
+        l.sort(reverse=True)
+        max_diff = max(max_diff, l[0]) # Update max_diff to the highest occurence of output difference for current input difference if the number is higher than the current value of max_diff. 
+    return max_diff
+
+# XOR the bits in list
+def xor(a, b):
+    return [x ^ y for x, y in zip(a, b)]
+
+# Convert list to string
+# From mandatory assignment 1 problem 2
+def bits_to_string(bits):
+    return ''.join(str(b) for b in bits)
+
+# Converts a binary string to a list of bits
+# From mandatory assignment 1 problem 2
+def string_to_bits(s):
+    return [int(b) for b in s]
+
+# Reads from lookup table file and creates lists of bits.
+def read_from_lookuptable(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        lookup_table_list = []
+        for line in lines:
+            input_str, output_str = line.strip().split('->')
+            lookup_table_list.append([string_to_bits(input_str), string_to_bits(output_str)])
+        f.close()
+    return lookup_table_list
+
+# Main method used to take in arguments from terminal and print the result
+def main():
+    print(differential_uniformity(read_from_lookuptable(sys.argv[1])))
+
+main()
